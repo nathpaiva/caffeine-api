2765785f7521704ced24bb2457bd1c0ce67d19dc
Nath Paiva <hello@nathpaiva.com.br>
2023-07-31 16:23:50 -0700

    feat: creates capsules model

---
+++ src/controllers/Capsules.ts
@@ -0,0 +1,70 @@
+import { validationResult } from 'express-validator';
+
+import Capsules from '../models/Capsules';
+
+type CapsuleBody = Omit<Capsule, 'create_date' | 'notify_end'> & {
+  notify_end_days_before?: Capsule['notify_end']['days_before'];
+  notify_end_active?: Capsule['notify_end']['active'];
+};
+
+export const capsules: Controllers['capsules'] = {
+  async list_capsules(_, res) {
+    const capsules = await Capsules.find();
+
+    return res.json({
+      success: true,
+      items: capsules,
+    });
+  },
+  async create(req, res) {
+    const { id } = req.params;
+    const response = validationResult(req);
+
+    if (response && !response.isEmpty()) {
+      return res.status(422).json({ errors: response.array() });
+    }
+
+    const capsules: CapsuleBody = req.body;
+
+    try {
+      const response = await Capsules.create<Omit<Capsule, 'create_date'>>({
+        user_id: id,
+        name: capsules.name,
+        brand: capsules.brand,
+        type: capsules.type,
+        price_last_buy: capsules.price_last_buy,
+        quantity_by_week: capsules.quantity_by_week,
+        notify_end: {
+          active: capsules.notify_end_active ?? false,
+          days_before: capsules.notify_end_days_before ?? 0,
+        },
+      });
+
+      return res.json({
+        success: true,
+        data: response,
+      });
+    } catch (err) {
+      return res.json({
+        success: false,
+        message: err,
+      });
+    }
+  },
+  async delete(req, res) {
+    const { id } = req.params;
+
+    try {
+      await Capsules.findByIdAndRemove(id);
+
+      return res.json({
+        success: true,
+      });
+    } catch (err) {
+      return res.json({
+        success: false,
+        message: err,
+      });
+    }
+  },
+};
---
+++ src/models/Capsules.ts
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+
+const capsulesInterfaceSchema = new mongoose.Schema<Capsule>({
+  user_id: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  brand: {
+    type: String,
+  },
+  type: {
+    type: String,
+  },
+  price_last_buy: {
+    type: Number,
+  },
+  quantity_by_week: {
+    type: Number,
+  },
+  notify_end: {
+    active: {
+      type: Boolean,
+      default: false,
+    },
+    days_before: {
+      type: Number,
+    },
+  },
+  create_date: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+export default mongoose.model('Capsules', capsulesInterfaceSchema);
